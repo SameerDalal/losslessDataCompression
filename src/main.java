da5cca0a228e0cb38a99cf3c97da7aec37486de8
SameerDalal <sameerdalal747@gmail.com>
2022-02-21 11:44:36 -0500

    added the initial and main Dictionary

--- src/main.java
+++ src/main.java
@@ -3,39 +3,54 @@ import java.util.Scanner;
 
 public class main {
     Scanner scan = new Scanner(System.in);
-    HashMap<Integer, String> userInputSplit = new HashMap<>();
+    HashMap<Integer, String> initialDictionary = new HashMap<>();
+    public static int counter = 0;
 
 
     public static void main(String[] args){
-
         main m = new main();
         m.goTo();
+
 
     }
 
     public void goTo(){
         String input = scan.next();
-        encode(addToArray(input));
-
+        mainDictionary(setInitialDictionary(addToArray(input)));
 
     }
 
     public static String[] addToArray(String userInput){
         String[] array = userInput.split("");
         return array;
-
     }
-    public void encode(String[] userArray){
+    public String[] setInitialDictionary(String[] userArray){
+
         for (int i = 0; i < userArray.length; i++){
-            if(userArray[i] == userInputSplit.get(userArray[i])){
-                
-            } else {
-                userInputSplit.put(i,userArray[i]);
+            if (!(initialDictionary.containsValue(userArray[i]))){
+                initialDictionary.put(counter, userArray[i]);
+                counter += 1;
+
             }
-            System.out.println(userInputSplit);
-
         }
+        System.out.println(initialDictionary);
+        return userArray;
+    }
 
-
+    public void mainDictionary(String[] userArray){
+        for (int z = 0; z < userArray.length; z++) {
+            String userString = userArray[z];
+            int counter1 = z;
+            while (initialDictionary.containsValue(userString)) {
+                System.out.println(userString);
+                if (!(counter1 == userArray.length+1)) {
+                    userString = userString + userArray[counter1 + 1];
+                    counter1 += 1;
+                } else {
+                    break;
+                }
+            }
+            initialDictionary.put(counter, userString);
+        }
     }
 }
