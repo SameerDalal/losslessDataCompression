112efee07ae66a625bfc220906f51798c133adb6
SameerDalal <sameerdalal747@gmail.com>
2022-02-27 11:50:56 -0500

    The method 'setInitialDictionary' works properly.

--- src/main.java
+++ src/main.java
@@ -1,8 +1,5 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class main {
     Scanner scan = new Scanner(System.in);
@@ -25,16 +22,20 @@ public class main {
 
     }
 
-    public static String[] addToArray(String userInput){
+    public static ArrayList<String> addToArray(String userInput){
         String[] array = userInput.split("");
-        return array;
+        ArrayList<String> list = new ArrayList<>();
+        for(int q = 0; q < array.length; q++){
+            list.add(array[q]);
+        }
+        return list;
     }
-    public String[] setInitialDictionary(String[] userArray){
+    public ArrayList<String> setInitialDictionary(ArrayList<String> userArray){
 
-        for (int i = 0; i < userArray.length; i++){
-            if (!(dictionary.containsValue(userArray[i]))){
-                dictionary.put(counter, userArray[i]);
-                initialDictionary.add(userArray[i]);
+        for (int i = 0; i < userArray.size(); i++){
+            if (!(dictionary.containsValue(userArray.get(i)))){
+                dictionary.put(counter, userArray.get(i));
+                initialDictionary.add(userArray.get(i));
                 counter += 1;
 
             }
@@ -43,16 +44,29 @@ public class main {
         return userArray;
     }
 
-    public void mainDictionary(String[] userArray) {
-        for (int z = 0; z < userArray.length-1; z++) {
-            String userString = userArray[z];
-            if (!(z >= userArray.length + 1)) {
+    public void mainDictionary(ArrayList<String> userArray) {
+        for (int z = 0; z < userArray.size()-1; z++) {
+            String userString = userArray.get(z);
             int counter1 = z + 1;
-            while (dictionary.containsValue(userString)) {
-                userString = userString + userArray[counter1];
-                System.out.println(userString);
-                counter1 += 1;
-            }
+                while (dictionary.containsValue(userString)) {
+                    if (counter1 != userArray.size()) {
+                        userString = userString + userArray.get(counter1);
+                        System.out.println(userString);
+                        counter1 += 1;
+
+                        String[] array = userString.split("");
+                        ArrayList<String> minList = new ArrayList<>();
+                        for(int q = 0; q < array.length; q++) {
+                            minList.add(array[q]);
+                        }
+                        if(minList.size() > 2){
+                            int subtractBy = minList.size() - 2;
+                            z = z+subtractBy;
+                        }
+            } else {
+                        break;
+                    }
+
         }
             dictionary.put(counter, userString);
             counter += 1;
@@ -62,6 +76,7 @@ public class main {
     }
 
     public void getEmbeddedOutput(){
+        System.out.println("wor5");
         for (int u = dictionary.size() + 1; u < counter; counter++){
             String temporaryString = initialDictionary.get(u);
             String[] splitString  = temporaryString.split("");
